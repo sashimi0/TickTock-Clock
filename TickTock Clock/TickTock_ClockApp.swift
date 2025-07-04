e827e7ff498ab6743099a07b8a9e4144116088d3
Sasha <6260727+sashimi0@users.noreply.github.com>
2025-07-03 19:53:46 -0700

    Initial Commit

---
+++ TickTock Clock/TickTock_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTock_ClockApp.swift
+//  TickTock Clock
+//
+//  Created by sasha on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TickTock_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TickTock ClockTests/TickTock_ClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  TickTock_ClockTests.swift
+//  TickTock ClockTests
+//
+//  Created by sasha on 7/3/25.
+//
+
+import Testing
+@testable import TickTock_Clock
+
+struct TickTock_ClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
